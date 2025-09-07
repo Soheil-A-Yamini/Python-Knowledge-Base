fc1522512f7881fa72112c723e4f49eb2b98b7be
Soheil A-Yamini <103450695+Soheil-A-Yamini@users.noreply.github.com>
2025-09-07 20:17:12 +0200

    Create list_examples.py with list examples

---
+++ list/list_examples.py
@@ -0,0 +1,11 @@
+# List Examples
+
+my_list = [1, 2, 3, 4]
+print("List:", my_list)
+
+print("Second element:", my_list[1])
+
+my_list.append(5)
+print("After append:", my_list)
+
+print("Slice [1:3]:", my_list[1:3])
